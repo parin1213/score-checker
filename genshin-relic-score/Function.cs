8feefcd3ec9daafb6bb88ab6991a9fbc668e0f8a
DESKTOP-DEVK5VU\schwa <parin1213@gmail.com>
2021-10-02 23:56:25 +0900

    初回コミット

---
+++ genshin-relic-score/Function.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using genshin.relic.score.Models;
+using Google.Cloud.Vision.V1;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon;
+using System.Security.Cryptography;
+using System.Threading;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace genshin_relic_score
+{
+    public class Function
+    {
+
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public LambdaResponse FunctionHandler(Request req, ILambdaContext context)
+        {
+            //------------------------------
+            // 応答電文の設定
+            //------------------------------
+            LambdaResponse response = createDefaultResponse();
+            var body = new Dictionary<string, object>();
+
+
+            try
+            {
+                //------------------------------
+                // 送信された画像を復号化
+                //------------------------------
+                var imageBinary = decryptBase64String(req.Body);
+                //var imageBinary = File.ReadAllBytes(@"C:\Users\schwa\Videos\Captures\relic\DDD844CF49F78ADBAEB064005E5BED52.png");
+
+                //------------------------------
+                // 応答固有のID(画像のハッシュ値)の生成
+                //------------------------------
+                using var hashProvider = MD5.Create();
+                var hash = string.Join("", hashProvider.ComputeHash(imageBinary).Select(b => b.ToString("X2")));
+
+                //------------------------------
+                // キャッシュ取得
+                //------------------------------
+                var cacheExists = tryGetCacheData(hash, body);
+                if (cacheExists) { return response; }
+
+                //------------------------------
+                // S3へ保存(画像)
+                //------------------------------
+                saveFIleForS3($"image/{hash}.png", imageBinary);
+
+                //------------------------------
+                // 画像認識
+                //------------------------------
+                var relic = new Relic(imageBinary);
+                var word_list = relic.detectWords();
+
+                //------------------------------
+                // サブステータス抽出
+                //------------------------------
+                var relicSubStatusList = relic.getRelicSubStatus(word_list);
+
+                //------------------------------
+                // S3へ保存(サブステータス)
+                //------------------------------
+                saveFIleForS3($"image/{hash}_status.json", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(relicSubStatusList)));
+
+                //------------------------------
+                // スコア計算
+                //------------------------------
+                double score = relic.calculateScore(relicSubStatusList);
+
+                //------------------------------
+                // S3へ保存(スコア)
+                //------------------------------
+                saveFIleForS3($"image/{hash}_score.json",
+                    Encoding.UTF8.GetBytes(
+                        JsonConvert.SerializeObject(
+                            new Dictionary<string, string>()
+                            {
+                            { "score", $"{score:0.0#}"}
+                            })
+                        ));
+
+                //------------------------------
+                // 応答設定
+                //------------------------------
+                body.Add("score", $"{score:0.0#}");
+                body.Add("sub_status", relicSubStatusList);
+            }
+            catch (Exception ex)
+            {
+                var dev_mode = req.QueryStringParameters["dev_mode"]?.ToString();
+                if (dev_mode == "1")
+                {
+                    body["body"] = req.Body;
+                    body["StackTrace"] = ex.ToString();
+                }
+            }
+            finally
+            {
+                response.body = JsonConvert.SerializeObject(body);
+            }
+            return response;
+        }
+
+        public byte[] decryptBase64String(string base64String)
+        {
+            var data = Convert.FromBase64String(base64String);
+            var dataString = Encoding.UTF8.GetString(data);
+            if (dataString.StartsWith("data:"))
+            {
+                // ブラウザから送信されるデータは"data:image/xxx, (base64文字列) "形式なので変換する
+                base64String = dataString.Split(",").ElementAtOrDefault(1)?.Replace("-", "+")?.Replace("_", "/");
+                data = Convert.FromBase64String(base64String);
+            }
+
+            return data;
+        }
+
+        private void saveFIleForS3(string filePath, byte[] bin)
+        {
+            var client = new AmazonS3Client(RegionEndpoint.APNortheast1);
+
+            PutObjectRequest request = new PutObjectRequest()
+            {
+
+                BucketName = "relic-server-log",
+                Key = filePath,
+
+                StorageClass = S3StorageClass.StandardInfrequentAccess,
+                CannedACL = S3CannedACL.Private,
+                InputStream = new MemoryStream(bin),
+            };
+            request?.InputStream?.Seek(0, SeekOrigin.Begin);
+
+            try
+            {
+                client.PutObjectAsync(request).Wait();
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        private LambdaResponse createDefaultResponse()
+        {
+            var response = new LambdaResponse()
+            {
+                isBase64Encoded = false,
+                statusCode = HttpStatusCode.OK,
+                headers = new Dictionary<string, string>()
+                {
+                    { "Access-Control-Allow-Origin",  "*" },
+                    { "Access-Control-Allow-Headers", "*" },
+                    { "Access-Control-Allow-Methods", "*" },
+                },
+            };
+
+            return response;
+        }
+
+        private bool tryGetCacheData(string hash, Dictionary<string, object> dic)
+        {
+            var client = new AmazonS3Client(RegionEndpoint.APNortheast1);
+
+            //------------------------------
+            // キャッシュ存在チェック
+            //------------------------------
+            var request = new ListObjectsV2Request
+            {
+                BucketName = "relic-server-log",
+                Prefix = $"image/{hash}",
+                MaxKeys = 3
+            };
+            var response = client.ListObjectsV2Async(request, CancellationToken.None).Result;
+            if (response.S3Objects.Count() != 3) { return false; }
+
+            //------------------------------
+            // キャッシュ取得
+            //------------------------------
+            var status = client.GetObjectAsync("relic-server-log", $"image/{hash}_status.json").Result;
+            var statusStream = new StreamReader(status.ResponseStream);
+            var statusDic = JsonConvert.DeserializeObject<Dictionary<string, double>>(statusStream.ReadToEnd());
+
+            var score = client.GetObjectAsync("relic-server-log", $"image/{hash}_score.json").Result;
+            var scoreStream = new StreamReader(score.ResponseStream);
+            var scoreDic = JsonConvert.DeserializeObject<Dictionary<string, string>>(scoreStream.ReadToEnd());
+
+            //------------------------------
+            // キャッシュ値格納
+            //------------------------------
+            dic["sub_status"] = statusDic;
+
+            foreach (var pair in scoreDic)
+            {
+                dic[pair.Key] = pair.Value;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ genshin-relic-score/Models/Relic.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Google.Cloud.Vision.V1;
+
+namespace genshin.relic.score.Models
+{
+    public class Relic
+    {
+        const string scretPath = @"./gcp.json";
+        public byte[] imageBinary;
+
+        public Relic(byte[] imageBinary)
+        {
+            //------------------------------
+            // 引数設定
+            //------------------------------
+            this.imageBinary = imageBinary;
+
+            //------------------------------
+            // GCPアカウントの設定
+            //------------------------------
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", scretPath);
+        }
+
+        public List<string> detectWords()
+        {
+            //------------------------------
+            // 画像のオブジェクトの生成
+            //------------------------------
+            var image = Image.FromBytes(imageBinary);
+
+            //------------------------------
+            // GCPへOCRリクエスト
+            //------------------------------
+            ImageAnnotatorClient client = ImageAnnotatorClient.Create();
+            TextAnnotation text = client.DetectDocumentText(image);
+
+            return text.Text.Split('\n').ToList();
+        }
+
+        public Dictionary<string, double> getRelicSubStatus(List<string> lines)
+        {
+            //------------------------------
+            // 正規表現パターン生成
+            //------------------------------
+            var status_classes = new[] { "攻撃力", "攻擊力", "防御力", "HP", "元素熟知", "元素チャージ効率", "会心率", "会心ダメージ" };
+            var status_class_pattern = string.Join("|", status_classes);
+            var numeric_pattern = "(?<value>([1-9]\\d*|0)(\\.\\d+)?(%)?)";
+            var pattern = $"(?<class>{status_class_pattern})(.*?)?" + "(\\+)?" + numeric_pattern;
+
+            //------------------------------
+            // パターンに一致する文字列を抽出
+            //------------------------------
+            var statusList = lines.Where(text => Regex.IsMatch(text, pattern));
+
+
+            //------------------------------
+            // パターンに一致しない場合
+            // （聖遺物強化画面のキャプチャの場合）
+            //------------------------------
+            bool isDetailPage = statusList.Any() == false;
+            if (isDetailPage)
+            {
+                // 聖遺物強化画面の場合はOCR結果が「攻撃力\n5.7%」のような形で取得されるので
+                // 数値っぽい値の場合前の行と合成してパターンを作る
+                statusList = lines.Select((t, i) =>
+                {
+                    if (i < 1) { return t; }
+
+                    if (Regex.IsMatch(t, numeric_pattern))
+                    {
+                        return statusList.ElementAt(i - 1) + "+" + t;
+                    }
+
+                    return t;
+                });
+            }
+
+            //------------------------------
+            // 最終フィルタ実施
+            // (セット効果等をフィルタ実施)
+            //------------------------------
+            statusList = statusList.Select(text =>
+                                    {
+                                        return (text: text, match: Regex.Match(text, pattern));
+                                    })
+                                    .Where(m =>
+                                    {
+                                        // 記号は無視する
+                                        var size = m.text
+                                                    .Where(c => char.IsSymbol(c) == false)
+                                                    .Where(c => char.IsPunctuation(c) == false)
+                                                    .Where(c => char.IsWhiteSpace(c) == false)
+                                                    .Count();
+                                        // セット効果の説明文などをフィルタするために、ステータスらしき箇所だけ抽出する
+                                        return ((double)m.match.Value.Length / size) > 0.8;
+                                    })
+                                    .Select(m => $"{m.match.Groups["class"]}+{m.match.Groups["value"]}")
+                                    .ToArray();
+
+            //------------------------------
+            // メイン効果を削除
+            //------------------------------
+            if (isDetailPage && statusList.Count() > 4)
+            {
+                statusList = statusList.TakeLast(4);
+            }
+
+            //------------------------------
+            // オプション毎にステータスを記録
+            //------------------------------
+            var dic = statusList.Distinct()
+               .Select(text => text.Split("+"))
+               .ToDictionary(
+                    t => t.ElementAtOrDefault(0) + (t.ElementAtOrDefault(1).EndsWith("%") ? "%" : ""),
+                    t => double.Parse(t.ElementAtOrDefault(1).Replace("%", "")));
+
+            dic = filterOptions(dic);
+
+            return dic;
+        }
+
+        private Dictionary<string, double> filterOptions(Dictionary<string, double> subStatusOption)
+        {
+            var scoreRates = new Dictionary<string, (double minValue, double maxValue)>()
+            {
+                // key              maxValue,  minValue
+                {"攻撃力",           ( 7.0d,  19d  * 6)},
+                {"攻撃力%",          ( 2.5d,  5.8d * 6)},
+                {"攻擊力%",          ( 2.5d,  5.8d * 6)},
+                {"防御力",           ( 8.0d,  23d  * 6)},
+                {"防御力%",          ( 3.1d,  7.3d * 6)},
+                {"HP",               ( 110d,  299  * 6)},
+                {"HP%",              ( 2.5d,  5.8d * 6)},
+                {"元素熟知",         (  10d,  23   * 6)},
+                {"元素チャージ効率", ( 2.7d,  6.5  * 6)},
+                {"会心率%",          ( 1.6d,  3.9  * 6)},
+                {"会心ダメージ%",    ( 3.3d,  7.8  * 6)},
+            };
+
+            // 中身をコピー
+            subStatusOption = new Dictionary<string, double>(subStatusOption);
+
+            foreach (var scoreRatePair in scoreRates)
+            {
+                var minValue = scoreRatePair.Value.minValue;
+                var maxValue = scoreRatePair.Value.maxValue;
+
+                // 無ければ次の検索へ
+                if (subStatusOption.TryGetValue(scoreRatePair.Key, out var value) == false) { continue; }
+
+                // 最低値以上、最大値未満でないものは削除する
+                if (value < minValue || maxValue < value)
+                {
+                    subStatusOption.Remove(scoreRatePair.Key);
+                }
+            }
+
+            return subStatusOption;
+        }
+
+        public double calculateScore(Dictionary<string, double> dic)
+        {
+            double score = 0;
+
+            var scoreRates = new Dictionary<string, double>()
+            {
+                {"攻撃力%",         1.0d},
+                {"攻擊力%",         1.0d},
+                {"会心率%",         2.0d},
+                {"会心ダメージ%",   1.0d},
+            };
+
+            foreach (var pair in scoreRates)
+            {
+                var key = pair.Key;
+                var rate = pair.Value;
+
+                if (dic.TryGetValue(key, out var strValue))
+                {
+                    score += strValue * rate;
+                }
+            }
+
+            return score;
+        }
+
+    }
+}
